da413ecc5a1dfb3adbbca35fd1b75dc4ce8e2a70
jackmarley254 <engndiritu1@gmail.com>
2023-08-08 23:46:03 +0300

    0x09-static_libraries tasks

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * Main - Entry point
+ * _puts: prints a string
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
