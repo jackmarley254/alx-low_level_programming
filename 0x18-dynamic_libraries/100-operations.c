b3fe735c7d028565b3e39a87989f2314b55b7ba4
jackmarley254 <engndiritu1@gmail.com>
2023-10-20 11:30:25 +0300

    Debugging

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -6,24 +6,57 @@ int mul(int a, int b);
 int div(int a, int b);
 int mod(int a, int b);
 
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ * Return: The sum of a and b.
+ */
 int add(int a, int b)
 {
-	return a + b;
+	return (a + b);
 }
 
+/**
+ * sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ * Return: The result of a minus b.
+ */
 int sub(int a, int b)
 {
-	return a - b;
+	return (a - b);
 }
+
+/**
+ * mul - multiply two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ * Return: The result of a minus b.
+ */
 int mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
+
+/**
+ * div - Divides two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ * Return: The result of a minus b.
+ */
 int div(int a, int b)
 {
-	return a / b;
+	return (a / b);
 }
+
+/**
+ * mod - remaider after dividing  two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ * Return: The result of a minus b.
+ */
 int mod(int a, int b)
 {
-	return a % b;
+	return (a % b);
 }
